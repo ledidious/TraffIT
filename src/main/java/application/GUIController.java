602e12b034e56b47bbb889b29f5db922f8c788ac
Jannis Brune <jannisbrune@t-online.de>
2018-11-07 13:28:09 +0100

    Initial Commit for GUI

---
+++ src/main/java/application/GUIController.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class GUIController implements Initializable {
+	@FXML
+	private Button button01;
+
+	@FXML
+	private TextField textfield01;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		// TODO (don't really need to do anything here).
+	}
+
+	// When user click on myButton
+	// this method will be called.
+	public void showDateTime(ActionEvent event) {
+		System.out.println("Button Clicked!");
+
+		Date now = new Date();
+
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
+
+		// Model Data
+		String dateTimeString = df.format(now);
+
+		// Show in VIEW
+		textfield01.setText(dateTimeString);
+
+	}
+}
