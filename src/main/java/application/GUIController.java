f2bf1a820df37b5fc91ca189cd2ffac492563e22
Jannis Brune <jannisbrune@t-online.de>
2018-11-14 18:54:52 +0100

    The first successful test.

--- src/main/java/application/GUIController.java
+++ src/main/java/application/GUIController.java
@@ -1,42 +1,22 @@
 package application;
 
-import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.ResourceBundle;
+import java.awt.Button;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
 
-public class GUIController implements Initializable {
+public class GUIController {
+	
 	@FXML
-	private HBox Lane1;
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-
-		// TODO (don't really need to do anything here).
+	private void handleDeletePerson() {
+	    System.out.println("Hier könnte Ihre Werbung stehen!");
 	}
-
+	
 	@FXML
-	protected void fillWithLane() {
-
-		ArrayList<Cell> CellsLane1 = new ArrayList<>();
-
-		for (int i = 0; i < 1900; i++) {
-
-			Cell cell = (new Cell(i + 10, 20, 1, 40, Color.BLACK));
-			CellsLane1.add(cell);
-		}
-		
-		fillWithLane();
+	private Label label01;	
+	
+	@FXML
+	private void changeTextFromLabel() {
+		label01.setText("Ich habe Lust auf Bier und Titten! ;)");
 	}
 }
--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,12 +2,10 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -15,25 +13,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-			
-					
-			/*			
-			ArrayList<Cell> CellsLane1 = new ArrayList<>();
-			ArrayList<Cell> CellsLane2 = new ArrayList<>();
-
-			for (int i = 0; i < 1900; i++) {
-				
-				Cell cell = (new Cell(i + 10, 20, 1, 40, Color.GREY));
-				CellsLane1.add(cell);
-				CellsLane2.add(cell);
-			*/
-			
-	
-			
-		
-			primaryStage.setTitle("TraffIT GUI");
-			primaryStage.setScene(new Scene(root));
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("GUI.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
