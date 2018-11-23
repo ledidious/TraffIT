60476ac1c3b6ffa0cd7b6349b2ace052b554c8a8
ledidious <ledidious@gmail.com>
2018-11-23 13:21:46 +0100

    Changed line encodings of all files to LF (Unix)

--- src/main/java/de/superdudes/traffit/application/Main.java
+++ src/main/java/de/superdudes/traffit/application/Main.java
@@ -1,61 +1,56 @@
 package de.superdudes.traffit.application;
-	
+
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.fxml.FXMLLoader;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("GUI.fxml"));
-			Scene scene = new Scene(root);
-			
-			//fixed window minimum size
-			root.setPrefSize(1280, 800);
-			primaryStage.setMinHeight(839);
-			primaryStage.setMinWidth(1296);
-			
-			
-			
-			
-			primaryStage.setFullScreen(true);
-			
-			//Listener to resize the window
-			scene.widthProperty().addListener(new ChangeListener<Number>() {
 
-				@Override
-				public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
-					System.out.println("Width: " + newSceneWidth);		
-					root.setPrefWidth((double) newSceneWidth);
-					
-				}
-				
-			});
-						
-			scene.heightProperty().addListener(new ChangeListener<Number>() {
-			    @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
-			        System.out.println("Height: " + newSceneHeight);
-			        root.setPrefHeight((double) newSceneHeight);
-			    }
-			});
-			
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start( Stage primaryStage ) {
+        try {
+            AnchorPane root = ( AnchorPane ) FXMLLoader.load( getClass().getResource( "GUI.fxml" ) );
+            Scene scene = new Scene( root );
+
+            //fixed window minimum size
+            root.setPrefSize( 1280, 800 );
+            primaryStage.setMinHeight( 839 );
+            primaryStage.setMinWidth( 1296 );
+
+            primaryStage.setFullScreen( true );
+
+            //Listener to resize the window
+            scene.widthProperty().addListener( new ChangeListener<Number>() {
+
+                @Override
+                public void changed( ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth ) {
+                    System.out.println( "Width: " + newSceneWidth );
+                    root.setPrefWidth( ( double ) newSceneWidth );
+                }
+            } );
+
+            scene.heightProperty().addListener( new ChangeListener<Number>() {
+                @Override
+                public void changed( ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight ) {
+                    System.out.println( "Height: " + newSceneHeight );
+                    root.setPrefHeight( ( double ) newSceneHeight );
+                }
+            } );
+
+            scene.getStylesheets().add( getClass().getResource( "application.css" ).toExternalForm() );
+            primaryStage.setScene( scene );
+            primaryStage.show();
+        }
+        catch( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main( String[] args ) {
+        launch( args );
+    }
 }
