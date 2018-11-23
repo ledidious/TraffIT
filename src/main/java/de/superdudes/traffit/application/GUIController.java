9b8e9b8519b70d5e484c800766f3f89fcbd1f236
marisbeautypalace <ma.gierke@gmail.com>
2018-11-23 12:15:41 +0100

    current status

--- src/main/java/de/superdudes/traffit/application/GUIController.java
+++ src/main/java/de/superdudes/traffit/application/GUIController.java
@@ -2,12 +2,15 @@ package de.superdudes.traffit.application;
 
 import java.io.File;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 public class GUIController {
@@ -35,6 +38,38 @@ public class GUIController {
 
 	@FXML
 	private Button button02;
+	
+	@FXML 
+	private Pane pane01;
+	
+	@FXML 
+	private void resizeButton() {
+		
+		//System.out.println(button01.getLayoutX());
+		//System.out.println(pane01.getParent().getLayoutX());
+		
+		
+		pane01.widthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldWidthValue, Number newWidthValue) {
+				//button01.setLayoutX((double)newWidthValue - button01.getWidth());
+				System.out.println(pane01.getBoundsInParent().getWidth());
+			}
+		});
+		
+		
+		pane01.heightProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldHeightValue, Number newHeightValue) {
+				//button01.setLayoutX((double)newHeightValue - button01.getHeight());	
+				System.out.println(pane01.getBoundsInParent().getHeight());
+			}
+		});
+		
+		
+	}
 
 	@FXML
 	private Label label01;
@@ -48,6 +83,7 @@ public class GUIController {
 		ivCar.setImage(iconCar);
 		ivMotorcycle.setImage(iconMotorcycle);
 		ivTruck.setImage(iconTruck);
+		resizeButton();
 	}
 	
 	String path = new File("src/main/resources/car.png").getAbsolutePath();
