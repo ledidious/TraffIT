f5793764b2623e72b90e31887132a574186e50ca
ledidious <ledidious@gmail.com>
2018-11-28 16:08:26 +0100

    Added missing @Getter/@Setter for SimulationObject

--- src/main/java/de/superdudes/traffit/dto/SimulationObject.java
+++ src/main/java/de/superdudes/traffit/dto/SimulationObject.java
@@ -1,9 +1,13 @@
 package de.superdudes.traffit.dto;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 import lombok.ToString;
 
+@Getter
+@Setter
 @EqualsAndHashCode( callSuper = true, of = "nr" )
 @ToString( of = { "nr" } )
 public class SimulationObject extends DatabaseObject {
