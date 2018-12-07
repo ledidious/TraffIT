e53ece0ef6b9b0757f1f165fc0f71bfb2dd21227
marisbeautypalace <ma.gierke@gmail.com>
2018-12-07 13:11:32 +0100

    Drag Over/Exit released: If you drag the bike over the lane or signlane you will see the position in form of a yellow rectangle(cell). If you leave it, this mark will removed.

--- src/main/java/de/superdudes/traffit/application/Cell.java
+++ src/main/java/de/superdudes/traffit/application/Cell.java
@@ -8,9 +8,14 @@ public class Cell extends Rectangle {
     public Cell( double x, double y, double width, double height, Paint fill ) {
         super( x, y, width, height );
         setFill( fill );
+        setOnMouseDragOver(e -> this.setFill(javafx.scene.paint.Color.YELLOW));
+        setOnMouseDragExited(e -> this.setFill(fill));
     }
 
     public Cell( double x, double y, double width, double height ) {
         super( x, y, width, height );
+
     }
+    
+    
 }
