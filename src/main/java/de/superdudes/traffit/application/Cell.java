f4e925f0be2d7db943eb48ed33a176d015bc5f0d
Jannis Brune <jannisbrune@t-online.de>
2018-12-12 15:41:41 +0100

    alpha version of drag&drop

--- src/main/java/de/superdudes/traffit/application/Cell.java
+++ src/main/java/de/superdudes/traffit/application/Cell.java
@@ -1,21 +1,43 @@
 package de.superdudes.traffit.application;
 
+import java.io.File;
+
+import javafx.event.EventHandler;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
 public class Cell extends Rectangle {
 
-    public Cell( double x, double y, double width, double height, Paint fill ) {
-        super( x, y, width, height );
-        setFill( fill );
-        setOnMouseDragOver(e -> this.setFill(javafx.scene.paint.Color.YELLOW));
-        setOnMouseDragExited(e -> this.setFill(fill));
-    }
 
-    public Cell( double x, double y, double width, double height ) {
-        super( x, y, width, height );
+	private ImageView currentImage;
 
-    }
-    
-    
+	public Cell(double x, double y, double width, double height, Paint fill) {
+		super(x, y, width, height);
+		setFill(fill);
+		setOnMouseDragOver(e -> this.setFill(javafx.scene.paint.Color.YELLOW));
+		setOnMouseDragExited(e -> this.setFill(fill));
+
+		setOnMouseDragReleased(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent event) {
+				Image iconCar = new Image("file:" + new
+				File("src/main/resources/car.png").getAbsolutePath());
+				
+				currentImage = new ImageView();
+				currentImage.setImage(iconCar);
+				currentImage.setId(this.getClass().getSimpleName() + System.currentTimeMillis());
+				
+				System.out.println("Event on Target: mouse drag released");
+			}
+		});
+
+	}
+
+	public Cell(double x, double y, double width, double height) {
+		super(x, y, width, height);
+
+	}
+
 }
