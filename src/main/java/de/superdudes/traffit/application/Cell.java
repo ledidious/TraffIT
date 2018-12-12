690a76558d975900f0737dbc2c020bd52eaa1002
Jannis Brune <jannisbrune@t-online.de>
2018-12-12 19:18:05 +0100

    drag&drop alpha v02 you can now drop sth. and it will change the color

--- src/main/java/de/superdudes/traffit/application/Cell.java
+++ src/main/java/de/superdudes/traffit/application/Cell.java
@@ -6,33 +6,30 @@ import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
 public class Cell extends Rectangle {
 
-
 	private ImageView currentImage;
+	private Boolean stopPainting = false;
 
 	public Cell(double x, double y, double width, double height, Paint fill) {
 		super(x, y, width, height);
 		setFill(fill);
 		setOnMouseDragOver(e -> this.setFill(javafx.scene.paint.Color.YELLOW));
-		setOnMouseDragExited(e -> this.setFill(fill));
-
-		setOnMouseDragReleased(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent event) {
-				Image iconCar = new Image("file:" + new
-				File("src/main/resources/car.png").getAbsolutePath());
-				
-				currentImage = new ImageView();
-				currentImage.setImage(iconCar);
-				currentImage.setId(this.getClass().getSimpleName() + System.currentTimeMillis());
-				
-				System.out.println("Event on Target: mouse drag released");
-			}
+		setOnMouseDragExited(e -> {
+			if (!stopPainting)
+				this.setFill(fill);
 		});
 
+		setOnMouseDragReleased(e -> {
+			this.setFill(javafx.scene.paint.Color.AQUA);
+			System.out.println("Hi Mario! Ich sende Text!");
+			this.stopPainting = true;
+		});
 	}
 
 	public Cell(double x, double y, double width, double height) {
