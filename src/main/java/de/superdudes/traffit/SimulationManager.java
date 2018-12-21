eed0e2e96e0e42dc8e09e9bb45c2c975d9ed112c
ledidious <ledidious@gmail.com>
2018-12-21 17:33:33 +0100

    Flag for gui to signalize that car was moved and has to be repainted (now in SimulationManager)

--- src/main/java/de/superdudes/traffit/SimulationManager.java
+++ src/main/java/de/superdudes/traffit/SimulationManager.java
@@ -1,25 +1,11 @@
 package de.superdudes.traffit;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.concurrent.Semaphore;
-
-import de.superdudes.traffit.controller.CellController;
-import de.superdudes.traffit.controller.ConstructionSiteController;
-import de.superdudes.traffit.controller.LaneController;
-import de.superdudes.traffit.controller.StartingGridController;
-import de.superdudes.traffit.controller.StreetController;
-import de.superdudes.traffit.controller.StreetSignController;
-import de.superdudes.traffit.controller.VehicleController;
-import de.superdudes.traffit.dto.Cell;
-import de.superdudes.traffit.dto.Lane;
-import de.superdudes.traffit.dto.StartingGrid;
-import de.superdudes.traffit.dto.Street;
-import de.superdudes.traffit.dto.StreetSign;
-import de.superdudes.traffit.dto.Vehicle;
+import de.superdudes.traffit.controller.*;
+import de.superdudes.traffit.dto.*;
 import de.superdudes.traffit.test.TestUtils;
 import lombok.NonNull;
+
+import java.util.concurrent.Semaphore;
 
 public class SimulationManager {
 
@@ -30,21 +16,15 @@ public class SimulationManager {
 	private static Thread executingThread = null;
 	private static Semaphore semaphore = new Semaphore(MAX_RUNNING_SIMULATION);
 
-	private static final StartingGridController STARTING_GRID_CONTROLLER = StartingGridController.instance();
-	private static final StreetController STREET_CONTROLLER = StreetController.instance();
-	private static final LaneController LANE_CONTROLLER = LaneController.instance();
-	private static final CellController CELL_CONTROLLER = CellController.instance();
-	private static final VehicleController VEHICLE_CONTROLLER = VehicleController.instance();
-	private static final StreetSignController STREET_SIGN_CONTROLLER = StreetSignController.instance();
-	private static final ConstructionSiteController CONSTRUCTION_SITE_CONTROLLER = ConstructionSiteController.instance();
+	private static boolean genWasRendered = false;
 
 	public static boolean load() {
-		runningSimulation = STARTING_GRID_CONTROLLER.load();
+		runningSimulation = StartingGridController.instance().load();
 		return runningSimulation != null;
 	}
 
 	public static boolean save() {
-		STARTING_GRID_CONTROLLER.save(runningSimulation);
+		StartingGridController.instance().save(runningSimulation);
 		return true;
 	}
 
@@ -74,7 +54,8 @@ public class SimulationManager {
 	}
 
 	public static boolean isRunning() {
-		return isStarted() && executingThread.holdsLock(semaphore);
+		// If executing thread waits for semaphore, semaphore has queued threads
+		return isStarted() && !semaphore.hasQueuedThreads();
 	}
 
 	private static void run() {
@@ -108,6 +89,7 @@ public class SimulationManager {
 				// Output current simulation
 				TestUtils.outputOnConsole(runningSimulation);
 
+				genWasRendered = true;
 				semaphore.release();
 			}
 		} catch (InterruptedException e) {
@@ -125,5 +107,13 @@ public class SimulationManager {
 		}
 
 		runningSimulation = startingGrid;
+	}
+
+	public static boolean isGenWasRendered() {
+		return genWasRendered;
+	}
+
+	public static void genWasRendered(boolean genWasRendered) {
+		SimulationManager.genWasRendered = genWasRendered;
 	}
 }
